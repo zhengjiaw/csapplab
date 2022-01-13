ee549f8e0a642a92e5349f7b93bc5665ff485399
linxi <zhengjia269@163.com>
2022-01-13 04:14:46 +0000

    完善CacheLab环境

--- lab5cachelab/install.sh
+++ lab5cachelab/install.sh
@@ -3,4 +3,6 @@ wget http://csapp.cs.cmu.edu/3e/README-"$lab"
 wget http://csapp.cs.cmu.edu/3e/"$lab".pdf
 wget "https://gitee.com/lin-xi-269/csapplab/raw/origin/lab5$lab/$lab""_【彩云小译】.pdf"
 wget http://csapp.cs.cmu.edu/3e/"$lab"-handout.tar
+wget http://csapp.cs.cmu.edu/public/waside/waside-blocking.pdf
+wget "https://gitee.com/lin-xi-269/csapplab/raw/origin/lab5$lab/waside-blocking彩云小译.pdf"
 tar -xvf *.tar
