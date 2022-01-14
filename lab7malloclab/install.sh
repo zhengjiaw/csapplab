eca68dd91bbcd2ebfcad7d3345e19c301128502e
linxi <zhengjia269@163.com>
2022-01-14 05:28:44 +0000

    完成自动化下载CSAPP8个lab

--- lab7malloclab/install.sh
+++ lab7malloclab/install.sh
@@ -11,4 +11,4 @@ wget "https://gitee.com/lin-xi-269/csapplab/raw/origin/lab7$lab/$lab""_【彩云
 wget http://csapp.cs.cmu.edu/3e/"$lab"-handout.tar
 tar -xvf $lab-handout.tar
 cd "$lab-handout"
-wget "https://gitee.com/lin-xi-269/csapplab/raw/origin/lab7$lab/traces.tar" && (tar -xvf traces.tar;rm traces.tar)
+wget "https://gitee.com/lin-xi-269/csapplab/raw/origin/lab7$lab/traces.tar.gz" && (tar -zxvf traces.tar.gz;rm traces.tar.gz)
