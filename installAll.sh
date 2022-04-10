3251810c92112f8f102fe501ab67980eeae8294e
林夕269 <zhengjia269@163.com>
2022-04-10 05:06:27 +0000

    update installAll.sh.

--- installAll.sh
+++ installAll.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+mkdir csapplab
+cd csapplab
 labs=( "datalab" "bomblab" "attacklab" "archlab" "cachelab" "shlab" "malloclab" "proxylab" )
 i=1
 for dir in ${labs[*]} ;do
@@ -7,3 +9,9 @@ for dir in ${labs[*]} ;do
     i=$((i+1))
 done
 
+# lab2 cgdb 工具
+# wget https://gitee.com/lin-xi-269/csapplab/raw/master/lab2bomb/installCgdb.sh
+# bash installCgdb.sh
+
+# lab4 环境配置
+# wget https://gitee.com/lin-xi-269/csapplab/raw/master/lab4archlab/archlab-handout/installTclTk.sh && bash installTclTk.sh
